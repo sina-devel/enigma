1f95498cebb7af41056aad9567b9cb1e85f6db06
sina-carbon <sina.carbon12@gmail.com>
2020-07-01 23:35:55 +0430

    start enigma

---
+++ src/main.rs
@@ -0,0 +1,122 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::process::exit;
+
+const ALPHABET: &str = "abcdefghijklmnopqrstuvwxyz ";
+
+struct Enigma {
+    state: u64,
+    rotors: (String, String, String),
+}
+
+impl Enigma {
+    fn new(rotors: (String, String, String)) -> Self {
+        Enigma {
+            state: 0,
+            rotors: rotors,
+        }
+    }
+
+    fn reflector(&self, c: char) -> char {
+        ALPHABET
+            .chars()
+            .nth(ALPHABET.len() - 1 - ALPHABET.find(c).unwrap())
+            .unwrap()
+    }
+
+    fn enigma_on_char(&mut self, c: char) -> char {
+        self.state += 1;
+        let c1 = self
+            .rotors
+            .0
+            .chars()
+            .nth(ALPHABET.find(c).unwrap())
+            .unwrap();
+        let c2 = self
+            .rotors
+            .1
+            .chars()
+            .nth(ALPHABET.find(c1).unwrap())
+            .unwrap();
+        let c3 = self
+            .rotors
+            .2
+            .chars()
+            .nth(ALPHABET.find(c2).unwrap())
+            .unwrap();
+        let reflected = self.reflector(c3);
+        let c3 = ALPHABET
+            .chars()
+            .nth(self.rotors.2.find(reflected).unwrap())
+            .unwrap();
+        let c2 = ALPHABET
+            .chars()
+            .nth(self.rotors.2.find(c3).unwrap())
+            .unwrap();
+        let c1 = ALPHABET
+            .chars()
+            .nth(self.rotors.2.find(c2).unwrap())
+            .unwrap();
+        c1
+    }
+
+    fn rotate_rotors(&mut self) {
+        self.rotors.0 = format!(
+            "{}{}",
+            self.rotors.0.get(1..).unwrap(),
+            self.rotors.0.chars().nth(0).unwrap()
+        );
+
+        if self.state % 27 == 0 {
+            self.rotors.1 = format!(
+                "{}{}",
+                self.rotors.1.get(1..).unwrap(),
+                self.rotors.1.chars().nth(0).unwrap()
+            );
+        }
+
+        if self.state % (27 * 27) == 0 {
+            self.rotors.2 = format!(
+                "{}{}",
+                self.rotors.2.get(1..).unwrap(),
+                self.rotors.2.chars().nth(0).unwrap()
+            );
+        }
+    }
+
+    fn code_decode(&mut self, plain: &str) -> String {
+        let mut cipher = String::new();
+        for c in plain.chars() {
+            self.state += 1;
+            cipher.push(self.enigma_on_char(c));
+            self.rotate_rotors();
+        }
+        cipher
+    }
+}
+
+fn read_rotors() -> (String, String, String) {
+    let file = File::open("today_rotors.enigma");
+    let file = match file {
+        Ok(file) => file,
+        Err(err) => {
+            eprintln!("error: {}", err);
+            exit(1);
+        }
+    };
+    let mut buf = BufReader::new(file);
+    let mut raw = String::new();
+    buf.read_to_string(&mut raw).unwrap();
+    let rotors: Vec<String> = raw.split("\n").map(|c| c.to_string()).collect();
+    let r1 = rotors[0].clone();
+    let r2 = rotors[1].clone();
+    let r3 = rotors[2].clone();
+    (r1, r2, r3)
+}
+
+fn main() {
+    let mut enigma = Enigma::new(read_rotors());
+    println!("hello world");
+    println!("{}", enigma.code_decode("hello world"));
+}
